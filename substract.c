e901540a4d67db58604086562fdfa033c1408794
Keerthana47 <82573455+Keerthana47@users.noreply.github.com>
2021-04-26 22:34:32 +0530

    Add files via upload

---
+++ substract.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,s;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+    s =a-b;
+    printf("%d",s);
+}   
