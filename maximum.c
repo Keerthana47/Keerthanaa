a8a8b2a672df59bd90491e3eab4f58d2fb012756
Keerthana47 <82573455+Keerthana47@users.noreply.github.com>
2021-04-28 20:19:38 +0530

    Add files via upload

---
+++ maximum.c
@@ -0,0 +1,20 @@
+// c program to find maximum between two numbers
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("Enter the two numbers:");
+    scanf("%d%d",&n1,&n2);
+    if(n1>n2)
+    {
+        printf("%d is maximum",n1);
+    }
+    if (n2>n1)
+    {
+        printf("%d is maximum",n2);
+    }
+    if
+    {
+        printf("Both are equal",n1,n2);
+    }
+}
